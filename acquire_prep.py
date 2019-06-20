262eb1c279d4b79a790bb56f510316894bce3954
Joseph Burton <josephburton06@gmail.com>
2019-06-20 16:37:01 -0500

    Add documentation on a_p

--- acquire_prep.py
+++ acquire_prep.py
@@ -6,6 +6,10 @@ def acquire_df():
     return df
 
 def prep_df():
+    '''
+    There is a column similar to an index that starts at 4.  It's in the 0th position. It has no purpose
+    and we'll drop it.
+    '''
     df = acquire_df()
     df.drop(df.columns[0], axis=1, inplace=True)
     return df
